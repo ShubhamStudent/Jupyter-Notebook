321427ee3dac8f46c0532d14c2ddda5fd1776a16
ShubhamStudent <150792898+ShubhamStudent@users.noreply.github.com>
2024-07-17 20:09:37 +0530

    Add files via upload

---
+++ Jupyter_Notebook.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # Introduction to Data Science Tools and Libraries
+# 
+# ## Introduction
+# Welcome to my Jupyterlite notebook showcasing various data science tools and libraries!
+# 
+# ### Data Science Languages
+# - Python
+# - R
+# - SQL
+# 
+# ### Data Science Libraries
+# - Pandas
+# - NumPy
+# - Matplotlib
+# - Scikit-learn
+# 
+# ### Data Science Tools Table
+# 
+# | Tool              | Description                            |
+# |-------------------|----------------------------------------|
+# | Jupyter Notebook  | Interactive computing environment       |
+# | TensorFlow        | Machine learning library                |
+# | Tableau           | Data visualization tool                 |
+# | SQL               | Database querying language              |
+# 
+# ## Arithmetic Expression Examples
+# Let's explore some basic arithmetic operations using Python.
+# 
+# # Multiply and add numbers
+# a = 5
+# b = 3
+# multiply_result = a * b
+# add_result = a + b
+# multiply_result, add_result
+# 
+# # Convert minutes to hours
+# minutes = 150
+# hours = minutes / 60
+# hours
+# 
+# 
+# ### Objectives
+# - Explore various data science tools and languages.
+# - Demonstrate basic arithmetic operations in Python.
+# - Convert units of time using Python.
+# 
+# ### Author
+# Shubham Bagwe
+# 
+
+# 
